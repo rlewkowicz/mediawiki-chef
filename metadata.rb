90b9fec0015b9cf24f8940da7552f064ce37afb0
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-07-07 15:59:19 -0400

    first commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'mediawiki'
+maintainer       'Ryan Lewkowicz'
+maintainer_email 'ryan.lewkowicz@spindance.com'
+license          'MIT'
+description      'Installs/Configures mediawiki'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'docker', '>= 2.9.2'
+depends 'ark', '~> 1.1'
---
+++ recipes/default.rb
@@ -0,0 +1,173 @@
+#
+# Cookbook Name:: mediawiki
+# Recipe:: default
+#
+# Maintainer: ryan.lewkowicz@spindance.com
+#
+
+#Initialize some varibles
+def random_password
+  require 'securerandom'
+  SecureRandom.base64
+end
+
+password = random_password
+datadir = node['mediawiki']['data_home']
+sitehome = node['mediawiki']['site_home']
+
+#create user for nginx/hhvm
+user node['mediawiki']['hhvm']['user']
+
+#create base dirs
+directory datadir do
+  recursive true
+end
+
+directory sitehome do
+  recursive true
+end
+
+#Set up Docker
+docker_installation 'default' do
+  repo 'test'
+  action :create
+end
+
+service "docker" do
+  action [ :enable, :start ]
+end
+
+docker_service 'default' do
+  daemon
+  action [:create, :start]
+end
+
+#Set up Maria 
+
+#So we have the client outside of the container
+package 'mariadb' 
+
+file '/root/.my.cnf' do
+  if !node['mycnf_set']
+    content lazy { "[mysql]\nuser=root\npassword=#{password}\n\n[mysqldump]\nuser=root\npassword=#{password}\n[client]\nprotocol=tcp" }
+    mode 0600
+    sensitive true
+  end
+end
+  
+ruby_block 'mycnf_set' do
+  block do
+    node.normal['mycnf_set'] = true
+    node.save
+  end
+end 
+
+docker_image "mariadb" do
+  action :pull
+  tag "#{node['mediawiki']['mariadb']['tag']}"
+end
+
+docker_container node['mediawiki']['mariadb']['container_name'] do
+  repo 'mariadb'
+  tag "#{node['mediawiki']['mariadb']['tag']}"
+  action :redeploy 
+  network_mode 'host'
+  #port node['mediawiki']['mariadb']['port']
+  env ["MYSQL_ROOT_PASSWORD=#{password}", "MYSQL_USER=root@#{node['ipaddress']}", "MYSQL_PASSWORD=#{password}"]
+  volumes [ "#{datadir}:#{datadir}", '/root/.my.cnf:/etc/mysql/conf.d/.my.cnf' ]
+  ignore_failure true
+end
+
+#Setup Nginx
+remote_directory '/etc/nginx'
+
+docker_image "nginx" do
+  action :pull
+  tag node['mediawiki']['nginx']['tag']
+end
+
+docker_container node['mediawiki']['nginx']['container_name'] do
+  repo 'nginx'
+  tag node['mediawiki']['nginx']['tag']
+  #port node['mediawiki']['nginx']['port']
+  network_mode 'host'
+  volumes [ "#{sitehome}:#{sitehome}", '/etc/nginx:/etc/nginx' ]
+  action :redeploy
+  ignore_failure true
+end
+
+#Setup HHVM
+docker_image "rlewkowicz/hhvm" do
+  action :pull
+  tag node['mediawiki']['hhvm']['tag']
+end
+
+docker_container 'remove hhvm' do
+  container_name node['mediawiki']['hhvm']['container_name']
+  action :delete
+  force true
+  ignore_failure true
+end
+
+docker_container node['mediawiki']['hhvm']['container_name'] do
+  repo 'rlewkowicz/hhvm'
+  tag node['mediawiki']['hhvm']['tag']
+  #port node['mediawiki']['hhvm']['port']
+  action :redeploy
+  network_mode 'host'
+  volumes "#{sitehome}:#{sitehome}" 
+  command "/usr/bin/hhvm --mode server \
+--config /etc/hhvm/php.ini \
+--config /etc/hhvm/server.ini \
+--user #{node['mediawiki']['hhvm']['user']} \
+-vPidFile=/var/redeploy/hhvm/pid \
+-d hhvm.repo.central.path = /tmp/hhvm.hhbc"
+  ignore_failure true
+  signal 'SIGKILL'
+end
+
+#Setup MediaWiki
+docker_image "rlewkowicz/mediawiki" do
+  action :pull
+  tag node['mediawiki']['tag']
+end
+  
+execute 'init_mediawiki' do
+    command 'docker cp mediawiki:/var/www/mediawiki /var/www/&&chown -R nginx.nginx /var/www/mediawiki'
+    action :nothing
+end
+
+docker_container "initalize media wiki" do
+  container_name node['mediawiki']['container_name']
+  repo 'rlewkowicz/mediawiki'
+  tag node['mediawiki']['tag']
+  #port node['mediawiki']['hhvm']['port']
+  action :redeploy
+  network_disabled true
+  tty true
+  command '/bin/bash'
+  ignore_failure true
+  notifies :run, 'execute[init_mediawiki]', :immediately
+  not_if { node['mediawiki_init'] }
+end  
+
+ruby_block 'mediawiki_init_set' do
+  block do
+    node.normal['mediawiki_init'] = true
+    node.save
+  end
+end 
+
+docker_container "media wiki continuence" do
+  container_name node['mediawiki']['container_name']
+  repo 'rlewkowicz/mediawiki'
+  tag node['mediawiki']['tag']
+  #port node['mediawiki']['hhvm']['port']
+  network_disabled true
+  action :redeploy
+  tty true
+  command '/bin/bash'
+  ignore_failure true
+  volumes "#{sitehome}:/var/www/mediawiki"
+  only_if { node['mediawiki_init'] }
+end  
