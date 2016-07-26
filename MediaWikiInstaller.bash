8a86903de77b5022286a1e5eec92d4338315b0f5
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-07-26 14:33:02 -0400

    better os check

--- MediaWikiInstaller.bash
+++ MediaWikiInstaller.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-python -mplatform | grep -qi Ubuntu && sudo apt-get update && sudo apt-get install -y build-essential git curl || sudo yum groupinstall -y "Development tools"&&yum install -y git curl
+python -mplatform | grep -qvi 'Ubuntu\|debian' && sudo yum groupinstall -y "Development tools"&&yum install -y git curl
+python -mplatform | grep -qi 'Ubuntu\|debian' && sudo apt-get update && sudo apt-get install -y build-essential git curl
 export PATH=/opt/chef/embedded/bin:$PATH
 curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -v 12.12.15
 mkdir /cookbooks
