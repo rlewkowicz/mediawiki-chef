326dd4172c945963188a5c012fa4611b9b6abd86
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-07-26 14:16:08 -0400

    fixed order

--- MediaWikiInstaller.bash
+++ MediaWikiInstaller.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
+python -mplatform | grep -qi Ubuntu && sudo apt-get update && sudo apt-get install -y build-essential git curl || sudo yum groupinstall -y "Development tools"&&yum install -y git curl
 export PATH=/opt/chef/embedded/bin:$PATH
 curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -v 12.12.15
-python -mplatform | grep -qi Ubuntu && sudo apt-get install -y build-essential git || sudo yum groupinstall -y "Development tools"&&yum install -y git
 mkdir /cookbooks
 cd /cookbooks
 knife cookbook site download mediawiki
@@ -10,3 +10,6 @@ cd mediawiki
 bundle install
 $(find /opt/chef/embedded/lib/ruby/gems/ | grep bin | grep berks$ | head -1) vendor /cookbooks
 chef-client --local-mode -o recipe['mediawiki']
+
+
+
