093145b57bae86697bd6eba61e05d06e0775b5e1
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-07-26 14:42:49 -0400

    root check

--- MediaWikiInstaller.bash
+++ MediaWikiInstaller.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 python -mplatform | grep -qvi 'Ubuntu\|debian' && sudo yum groupinstall -y "Development tools"&&yum install -y git curl
 python -mplatform | grep -qi 'Ubuntu\|debian' && sudo apt-get update && sudo apt-get install -y build-essential git curl
 export PATH=/opt/chef/embedded/bin:$PATH
