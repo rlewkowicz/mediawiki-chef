1314a4e862747a178791750887379493a1168be3
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-07-27 17:45:51 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.lewkowicz@spindance.com'
 license          'MIT'
 description      'Installs/Configures mediawiki'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.7'
+version          '1.0.8'
 
 depends 'docker', '>= 2.9.2'
 depends 'ark'
