f48b20d6aaa5af1af4829b73823aa745c1554afd
rlewkowicz <ryan.lewkowicz@spindance.com>
2016-08-18 13:47:40 -0400

    metabump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.lewkowicz@spindance.com'
 license          'MIT'
 description      'Installs/Configures mediawiki'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.12'
+version          '1.0.13'
 
 depends 'docker', '>= 2.9.2'
 depends 'ark'
